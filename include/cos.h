145d0e1b1ead92664e8640de8997d8ae3701b2d6
miaaaaaaaaao <84165410+miaaaaaaaaao@users.noreply.github.com>
2021-06-30 11:04:19 +0800

    修改了cos函数，优化了界面，新增test部分
    修改了cos函数，使其可以进行异常数据的处理，优化了界面，新增test部分

---
+++ include/cos.h
@@ -0,0 +1,13 @@
+#ifndef COS_H
+#define COS_H
+#include <qstring.h>
+
+
+class Cos
+{
+public:
+    Cos();
+    QString CinTest(QString sIn);
+};
+
+#endif // COS_H
---
+++ include/sin.h
@@ -0,0 +1,12 @@
+#ifndef SIN_H
+#define SIN_H
+#include <qstring.h>
+
+class Sin
+{
+public:
+    Sin();
+    QString SinTest(QString sIn);
+};
+
+#endif // SIN_H
